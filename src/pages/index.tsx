40f9855d013fe359a0fd92d35bafc9461c8bdaf0
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2022-09-08 20:25:40 +0300

    add typescript and home page file

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>
+          Džiugas Pečiulevičius - Software engineer, designer, and aspiring
+          pilot
+        </title>
+        <meta
+          name="description"
+          content="I’m Džiugas, a software engineer, designer and aspiring pilot based in Vilnius City."
+        />
+      </Head>
+    </>
+  )
+}
+
+export default Home
