e1b31befdc31a01603f2ad5fda686fb0b022b037
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2022-09-10 14:52:18 +0300

    update tailwind globals css and add footer component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { Container } from '@/components/Container';
 
 export default function Home() {
   return (
@@ -10,10 +11,16 @@ export default function Home() {
         </title>
         <meta
           name="description"
-          content="I’m Džiugas, a software engineer, aviation enthusiast and aspiring pilot based in Vilnius City."
+          content="I’m Džiugas, a software engineer, designer, and aspiring pilot based in Vilnius City."
         />
       </Head>
-      <div>hello</div>
+      <Container className="mt-9">
+        <div className="max-w-2xl">
+          <h1 className="text-4xl font-bold tracking-tight text-amber-500 text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+            Software engineer, designer, and aspiring pilot.
+          </h1>
+        </div>
+      </Container>
     </>
   );
 }
