61a2dd4a1635e2902c7ba081641e2b113b390a38
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2022-09-17 19:55:36 +0300

    fix nav items

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -106,10 +106,10 @@ function DesktopNavigation(props: any) {
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         <NavItem href="/about">About</NavItem>
-        <NavItem href="/articles">Articles</NavItem>
-        <NavItem href="/projects">Projects</NavItem>
-        <NavItem href="/speaking">Speaking</NavItem>
-        <NavItem href="/uses">Uses</NavItem>
+        <NavItem href="/blog">Blog</NavItem>
+        <NavItem href="/guestbook">Guestbook</NavItem>
+        <NavItem href="/snippets">Snippets</NavItem>
+        <NavItem href="/gear">Gear</NavItem>
       </ul>
     </nav>
   );
