cf6e05d66426b296f1cecd7dca76547920bd8b65
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2022-09-18 16:57:00 +0300

    add 'under development' messages whenever a page isn't complete

---
+++ src/pages/gear.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import { SimpleLayout } from '@/layouts/SimpleLayout';
+
+export default function Gear() {
+  return (
+    <>
+      <Head>
+        <title>Gear - Džiugas Pečiulevičius</title>
+        <meta name="description" content="This page is under development..." />
+      </Head>
+      <SimpleLayout title="Gear" intro="This page is under development...">
+        <div></div>
+      </SimpleLayout>
+    </>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -95,8 +95,12 @@ export default function Home() {
       </Container>
 
       <Photos />
+
       <Container className="mt-24 md:mt-28">
         <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
+          <div className="flex hidden flex-col gap-16 lg:block">
+            This section is under development...
+          </div>
           <div className="space-y-10 lg:pl-16 xl:pl-24">
             <Newsletter />
             <Resume />
