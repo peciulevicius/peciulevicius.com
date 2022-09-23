b78cb680be51291496539549b98e272e92966bdc
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2022-09-23 22:37:58 +0300

    add section component and add meta tag for gear page

--- src/pages/gear.tsx
+++ src/pages/gear.tsx
@@ -1,12 +1,38 @@
 import Head from 'next/head';
 import { SimpleLayout } from '@/layouts/SimpleLayout';
+import { Card } from '@/components/Card';
+import { Section } from '@/components/Section';
+
+function ToolsSection({ children, ...props }: any) {
+  return (
+    <Section {...props}>
+      <ul role="list" className="space-y-16">
+        {children}
+      </ul>
+    </Section>
+  );
+}
+
+function Tool({ title, href, children }: any) {
+  return (
+    <Card as="li">
+      <Card.Title as="h3" href={href}>
+        {title}
+      </Card.Title>
+      <Card.Description>{children}</Card.Description>
+    </Card>
+  );
+}
 
 export default function Gear() {
   return (
     <>
       <Head>
         <title>Gear - Džiugas Pečiulevičius</title>
-        <meta name="description" content="This page is under development..." />
+        <meta
+          name="description"
+          content="Software I use, gadgets I love, and other things I recommend."
+        />
       </Head>
       <SimpleLayout title="Gear" intro="This page is under development...">
         <div></div>
