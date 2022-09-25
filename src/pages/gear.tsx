03d8833e1edd79496b211a5c486a496ef3513216
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2022-09-25 11:47:46 +0300

    add gear page with dummy data

--- src/pages/gear.tsx
+++ src/pages/gear.tsx
@@ -34,8 +34,86 @@ export default function Gear() {
           content="Software I use, gadgets I love, and other things I recommend."
         />
       </Head>
-      <SimpleLayout title="Gear" intro="This page is under development...">
-        <div></div>
+      <SimpleLayout
+        title="Software I use, gadgets I love, and other things I recommend."
+        intro="Here’s a list of my favorite tech I'm currently using for coding."
+      >
+        <div className="space-y-20">
+          <ToolsSection title="Workstation">
+            <Tool title="MacBook Air M1, 16GB (2020)">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Tool>
+            <Tool title="HP 24f 24-inch Display">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud.
+            </Tool>
+            <Tool title="Keychron K2v2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Tool>
+            <Tool title="Razer Abyssus 1800">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Tool>
+          </ToolsSection>
+          <ToolsSection title="Development tools">
+            <Tool title="WebStorm | Neovim">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Tool>
+            <Tool title="iTerm2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Tool>
+            <Tool title="TablePlus">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Tool>
+          </ToolsSection>
+          <ToolsSection title="Design">
+            <Tool title="Figma">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </Tool>
+          </ToolsSection>
+          <ToolsSection title="Software">
+            <Tool title="Raycast">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Tool>
+            <Tool title="Reflect">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Tool>
+            <Tool title="SavvyCal">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Tool>
+            <Tool title="Focus">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Tool>
+          </ToolsSection>
+        </div>{' '}
       </SimpleLayout>
     </>
   );
