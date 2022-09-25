8f60ed00c07acd3e85aff5139c9f1306b9b2ab7a
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2022-09-25 11:54:56 +0300

    Gear page (#13)
    * fix newsletter input color on hover
    
    * add section component and add meta tag for gear page
    
    * add gear page with dummy data

--- src/pages/gear.tsx
+++ src/pages/gear.tsx
@@ -1,15 +1,119 @@
 import Head from 'next/head';
 import { SimpleLayout } from '@/layouts/SimpleLayout';
+import { Card } from '@/components/Card';
+import { Section } from '@/components/Section';
+
+function ToolsSection({ children, ...props }: any) {
+  return (
+    <Section {...props}>
+      <ul role="list" className="space-y-16">
+        {children}
+      </ul>
+    </Section>
+  );
+}
+
+function Tool({ title, href, children }: any) {
+  return (
+    <Card as="li">
+      <Card.Title as="h3" href={href}>
+        {title}
+      </Card.Title>
+      <Card.Description>{children}</Card.Description>
+    </Card>
+  );
+}
 
 export default function Gear() {
   return (
     <>
       <Head>
         <title>Gear - Džiugas Pečiulevičius</title>
-        <meta name="description" content="This page is under development..." />
+        <meta
+          name="description"
+          content="Software I use, gadgets I love, and other things I recommend."
+        />
       </Head>
-      <SimpleLayout title="Gear" intro="This page is under development...">
-        <div></div>
+      <SimpleLayout
+        title="Software I use, gadgets I love, and other things I recommend."
+        intro="Here’s a list of my favorite tech I'm currently using for coding."
+      >
+        <div className="space-y-20">
+          <ToolsSection title="Workstation">
+            <Tool title="MacBook Air M1, 16GB (2020)">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Tool>
+            <Tool title="HP 24f 24-inch Display">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud.
+            </Tool>
+            <Tool title="Keychron K2v2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Tool>
+            <Tool title="Razer Abyssus 1800">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Tool>
+          </ToolsSection>
+          <ToolsSection title="Development tools">
+            <Tool title="WebStorm | Neovim">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Tool>
+            <Tool title="iTerm2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Tool>
+            <Tool title="TablePlus">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Tool>
+          </ToolsSection>
+          <ToolsSection title="Design">
+            <Tool title="Figma">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </Tool>
+          </ToolsSection>
+          <ToolsSection title="Software">
+            <Tool title="Raycast">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Tool>
+            <Tool title="Reflect">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Tool>
+            <Tool title="SavvyCal">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Tool>
+            <Tool title="Focus">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Tool>
+          </ToolsSection>
+        </div>{' '}
       </SimpleLayout>
     </>
   );
