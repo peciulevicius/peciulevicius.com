bdd997f18bdb0f1a714562593e6ae90e6fb2b6d3
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2022-10-11 21:21:14 +0300

    list out snippets

--- src/layouts/CodeSnippetsLayout.tsx
+++ src/layouts/CodeSnippetsLayout.tsx
@@ -1,8 +1,9 @@
-// todo: fix 'any' type
 import Head from 'next/head';
+import Image from 'next/future/image';
 import { Container } from '@/components/Container';
 import { Prose } from '@/components/Prose';
 
+// todo: fix 'any' type
 export function CodeSnippetsLayout({ children, meta, isRssFeed = false }: any) {
   if (isRssFeed) {
     return children;
@@ -14,18 +15,30 @@ export function CodeSnippetsLayout({ children, meta, isRssFeed = false }: any) {
         <title>{`${meta.title} - Džiugas Pečiulevičius`}</title>
         <meta name="description" content={meta.description} />
       </Head>
+
       <Container className="mt-16 lg:mt-32">
         <div className="xl:relative">
-          <div className="mx-auto max-w-2xl">
-            <article>
-              <header className="flex flex-col">
-                <h1 className="mt-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-                  {meta.title}
-                </h1>
-              </header>
-              <Prose className="mt-8">{children}</Prose>
-            </article>
+          <div className="flex justify-between">
+            <header className="max-w-2xl">
+              <h1 className="mr-2 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+                {meta.title}
+              </h1>
+              <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+                {meta.description}
+              </p>
+            </header>
+            <div className="mt-2">
+              <Image
+                alt="logo"
+                height={60}
+                width={60}
+                src={meta.logo}
+                className="rounded-full"
+              />
+            </div>
           </div>
+
+          <Prose className="mt-8">{children}</Prose>
         </div>
       </Container>
     </>
