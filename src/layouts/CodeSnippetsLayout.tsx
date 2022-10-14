f832c1e3c981b44438deebb9a14bfe71676bb779
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2022-10-14 19:41:35 +0300

    update layout for code snippets

--- src/layouts/CodeSnippetsLayout.tsx
+++ src/layouts/CodeSnippetsLayout.tsx
@@ -18,27 +18,31 @@ export function CodeSnippetsLayout({ children, meta, isRssFeed = false }: any) {
 
       <Container className="mt-16 lg:mt-32">
         <div className="xl:relative">
-          <div className="flex justify-between">
-            <header className="max-w-2xl">
-              <h1 className="mr-2 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-                {meta.title}
-              </h1>
-              <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-                {meta.description}
-              </p>
-            </header>
-            <div className="mt-2">
-              <Image
-                alt="logo"
-                height={60}
-                width={60}
-                src={meta.logo}
-                className="rounded-full"
-              />
-            </div>
-          </div>
+          <div className="mx-auto max-w-2xl">
+            <article>
+              <div className="flex justify-between">
+                <header className="max-w-lg">
+                  <h1 className="mr-2 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+                    {meta.title}
+                  </h1>
+                  <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+                    {meta.description}
+                  </p>
+                </header>
+                <div className="mt-2">
+                  <Image
+                    alt="logo"
+                    height={60}
+                    width={60}
+                    src={meta.logo}
+                    className="rounded-full"
+                  />
+                </div>
+              </div>
 
-          <Prose className="mt-8">{children}</Prose>
+              <Prose className="mt-8">{children}</Prose>
+            </article>
+          </div>
         </div>
       </Container>
     </>
