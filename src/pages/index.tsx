29c713c1e0b206b3fad6d369c74a36a4b7f00569
Džiugas Pečiulevičius <43075730+peciulevicius@users.noreply.github.com>
2022-11-12 15:08:25 +0200

    fix gradients throwing out errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,8 @@
 import Head from 'next/head';
 import Hero from '@/components/Hero';
-import {
-  LinearGradientBottomDark,
-  LinearGradientBottomLight,
-  LinearGradientTopDark,
-  LinearGradientTopLight
-} from '@/components/Icons';
-import { useTheme } from 'next-themes';
+import { LinearGradientBottom, LinearGradientTop } from '@/components/Icons';
 
 export default function Home() {
-  const { resolvedTheme } = useTheme();
-
   return (
     <>
       <Head>
@@ -23,18 +15,10 @@ export default function Home() {
         />
       </Head>
       <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
-        {resolvedTheme === 'dark' ? (
-          <LinearGradientTopDark className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]" />
-        ) : (
-          <LinearGradientTopLight className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]" />
-        )}
+        <LinearGradientTop className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]" />
       </div>
       <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
-        {resolvedTheme === 'dark' ? (
-          <LinearGradientBottomDark className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]" />
-        ) : (
-          <LinearGradientBottomLight className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]" />
-        )}
+        <LinearGradientBottom className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]" />
       </div>
 
       <Hero />
