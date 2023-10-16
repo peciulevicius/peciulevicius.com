94f82ef283ec1dc2c4ea919036f84f9134b28869
Džiugas Pečiulevičius <43075730+peciulevicius@users.noreply.github.com>
2023-10-16 17:58:20 +0300

    add about section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -76,6 +76,7 @@ function MobileNavigation(props: { className: string }) {
             </div>
             <nav className="mt-6">
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
+                <MobileNavItem href="/about">About</MobileNavItem>
                 <MobileNavItem href="/blog">Blog</MobileNavItem>
                 <MobileNavItem href="/guestbook">Guestbook</MobileNavItem>
                 <MobileNavItem href="/snippets">Snippets</MobileNavItem>
@@ -116,6 +117,7 @@ function DesktopNavigation(props: { className: string }) {
   return (
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+        <NavItem href="/about">About</NavItem>
         <NavItem href="/blog">Blog</NavItem>
         <NavItem href="/guestbook">Guestbook</NavItem>
         <NavItem href="/snippets">Snippets</NavItem>
